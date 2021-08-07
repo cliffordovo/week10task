7a5d783c35ff07703de7180096c2ffdbb6ef1f8c
cliffordovo <86098916+cliffordovo@users.noreply.github.com>
2021-08-07 11:29:15 +0100

    done

---
+++ src/about.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header>
+       <nav>
+         <ul>
+           Home
+           About
+           Login
+           Contact
+         </ul>
+
+       </nav>
+     </header>
+
+     <h1>FRONTEND DEVELOPMENT</h1>
+     <button>MORE</button>
+
+     <h3>THIS IS THE HOME PAGE</h3>
+    </div>
+  );
+}
+
+export default App;
